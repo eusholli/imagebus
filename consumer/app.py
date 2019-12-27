bd367533a706604b25018c252ac85594db71310a
Geoff Hollingworth <eusholli@gmail.com>
2019-12-27 09:36:34 -0600

    step up fkask to latest, remove flask-appconfig

--- consumer/app.py
+++ consumer/app.py
@@ -1,5 +1,4 @@
 from flask_socketio import SocketIO, emit
-from flask_appconfig import AppConfig
 from flask import Flask, Response, render_template, flash
 from kafka import KafkaConsumer
 import umsgpack
@@ -10,13 +9,6 @@ import base64
 
 def create_app(configfile=None):
     app = Flask(__name__)
-    AppConfig(app, configfile)  # Flask-Appconfig is not necessary, but
-    # highly recommend =)
-    # https://github.com/mbr/flask-appconfig
-    # in a real app, these should be configured through Flask-Appconfig
-    app.config["SECRET_KEY"] = "devkey"
-    app.config["RECAPTCHA_PUBLIC_KEY"] = "6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw"
-
     return app
 
 
