9f6c994e148a149381a26abbd24e062cdc698183
Geoff Hollingworth <eusholli@gmail.com>
2019-12-26 19:48:57 -0600

    Make one universal installation file

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+echo "Installing consumer"
+cd consumer
+
+virtualenv venv
+
+. venv/bin/activate
+
+pip install -r requirements.txt
+
+deactivate
+
+cd ..
+
+echo "Installing producer"
+cd producer
+
+virtualenv venv
+
+. venv/bin/activate
+
+pip install -r requirements.txt
+
+deactivate
+
+cd ..
+
+echo "Installing imageai"
+cd imageai
+
+virtualenv venv
+
+. venv/bin/activate
+
+pip install -r requirements.txt
+curl -L -o yolo.h5 https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5
+
+deactivate
+
+cd ..
+
+echo "Installation Complete"
+
+
